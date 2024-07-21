0a320cc17a9fded9683f9dfa8662fc5548531148
hnthap <94937751+hnthap@users.noreply.github.com>
2024-07-21 23:48:03 +0700

    major change: move src files to src update gitignore to ignore bin, not out update README

--- README.md
+++ README.md
@@ -4,4 +4,85 @@ Converting from Gregorian calendar to Vietnamese Lunar calendar (change `TZ` in 
 
 Astronomical algorithms from [Ho Ngoc Duc's work](https://www.informatik.uni-leipzig.de/~duc/amlich/calrules.html).
 
-I re-wrote the algorithms in C to implement an easy-to-use CLI application.
+I re-wrote the algorithms in C to implement an easy-to-use CLI application. This requires a compiler that supports C99 or later.
+
+## Compilation
+
+In my case, I use gcc 8.3.0, but any other compilers that support C99 may work just fine (have not tested yet).
+
+```bash
+# Change directory to the directory containing this README
+# then run this
+
+gcc -Wpedantic -g src/lunar.c -o bin/lunar
+```
+
+## Arguments
+
+* `-tz`: Time zone in hours, e.g. `-tz 7` (default) is for UTC+07:00, `-tz 8` is for UTC+08:00.
+* `-y`: Year, e.g. `-y 2024` is the year 2024.
+* `-m`: Month from 1 to 12, e.g. `-m 7` is July (Gregorian calendar) or the 7th month (Lunar calendar).
+* `-d`: Day of month, from 1 to 31, e.g. `-d 21` is the 21st day of the month.
+* `-l`: (Lunar calendar only) Whether the month is leap. When `-l` does not present, the month is set as not leap, otherwise leap.
+* `-lm`: (Only when displaying Lunar calendar) Display the date in Lunar calendar in "modern notation", e.g.
+  * 2024.06.16: The **16th** day of the **6th** month (**non-leap**) of the Lunar year whose New Year falls in the Gregorian year **2024**.
+  * 2025.06+.01: The **1st** day of the **6th** month (**leap**) of the Lunar year whose New Year falls in the Gregorian year **2025**.
+* `-r`: Converting the date specified by `-y`, `-m`, `-l` and `-d` in Lunar calendar to Gregorian.
+* `-ly`: Print the first days of all months in the Lunar year specified by `-y` along with their equivalent Gregorian date.
+
+## Usage
+
+Convert date from Gregorian calendar to Lunar calendar:
+
+```bash
+./bin/lunar 
+# Output: "甲辰年六月（小）十六日"
+# Explain: Today in Lunar calendar.
+# Set "-tz 8" for Chinese calendar.
+
+./bin/lunar -lm
+# Output: "2024.06.16" 
+# Explain: Today in Lunar calendar - modern notation.
+# Set "-tz 8" for Chinese calendar.
+
+./bin/lunar -y 2025 -m 7 -d 30
+# Output: "乙巳年閏六月（小）初六日" 
+# Explain: 2025-07-30 in Gregorian calendar to Lunar.
+# Set "-tz 8" for Chinese calendar.
+
+./bin/lunar -y 2025 -m 7 -d 30 -lm
+# Output: "2025.06+.06"
+# Explain: 2025-07-30 in Gregorian calendar to Lunar - modern notation.
+# Set "-tz 8" for Chinese calendar.
+```
+
+Convert date from Lunar calendar to Gregorian calendar:
+
+```bash
+./bin/lunar -r -y 2025 -m 6 -l -d 6
+# Output: 2025.07.30
+# Explain: Lunar date 2025.06+.06 is Gregorian date 2025.07.30 in time zone UTC+07:00 (Vietnamese calendar).
+# Set "-tz 8" for Chinese calendar.
+```
+
+List first days of all months in a Lunar year with their equivalent Gregorian dates:
+
+```bash
+./bin/lunar -ly -y 2025            
+# Output:
+# Lunar   Gregorian
+# 2025.01.01      2025.01.29
+# 2025.02.01      2025.02.28
+# 2025.03.01      2025.03.29
+# 2025.04.01      2025.04.28
+# 2025.05.01      2025.05.27
+# 2025.06.01      2025.06.25
+# 2025.06+.01     2025.07.25
+# 2025.07.01      2025.08.23
+# 2025.08.01      2025.09.22
+# 2025.09.01      2025.10.21
+# 2025.10.01      2025.11.20
+# 2025.11.01      2025.12.20
+# 2025.12.01      2026.01.19
+```
+
--- src/lunar.c
+++ src/lunar.c
@@ -5,70 +5,43 @@
 #include <time.h>
 #include "lunar.h"
 
-const double TZ = 7.0;
-const double PI = 3.1415926535898;
-const double TAU = 2 * PI;
-const double DEGREES_TO_RADIANS = PI / 180.0;
-const char* BRANCHES[] = {
+const double g_pi = 3.1415926535898;
+const double g_tau = 2 * g_pi;
+const double g_degrees_to_radians = g_pi / 180.0;
+const char* g_branches[] = {
     "申", "酉", "戌", "亥", "子", "丑", "寅", "卯", "辰", "巳", "午", "未"
 };
-const char* STEMS[] = {
+const char* g_stems[] = {
     "庚", "辛", "壬", "癸", "甲", "乙", "丙", "丁", "戊", "己"
 };
 
 int main(int argc, char** argv)
 {
-    if (argc == 1)
+    struct ArgumentList argl = parse_argv(argc, argv);
+    if (argl.valid == 0)
     {
-        time_t raw;
-        time(&raw);
-        struct tm* info = localtime(&raw);
-        struct Gregorian date = new_Gregorian(
-            info->tm_year + 1900, 
-            info->tm_mon + 1, 
-            info->tm_mday,
-            TZ
-        );
-        struct Lunar lunar = convert_Gregorian_to_Lunar(date);
-        char out[256];
-        stringify_Lunar_date(lunar, out, 256);
-        printf("%s\n", out);
+        printf("Invalid arguments\n");
+        return 1;
     }
-    else if (argc == 4)
+    if (argl.task == LUNARH_GREGORIAN_TO_LUNAR_IN_CHINESE)
     {
-        int year = strtod(argv[1], NULL);
-        int month = strtod(argv[2], NULL);
-        int day = strtod(argv[3], NULL);
-        struct Gregorian date = new_Gregorian(year, month, day, TZ);
-        if (!date.valid)
-        {
-            printf("Invalid Gregorian date: %d-%d-%d\n", year, month, day);
-            return 1;
-        }
-        struct Lunar lunar = convert_Gregorian_to_Lunar(date);
-        char out[256];
-        stringify_Lunar_date(lunar, out, 256);
-        printf("%s\n", out);
+        display_Lunar_in_Chinese(argl);
     }
-    else if (argc == 5)
+    else if (argl.task == LUNARH_GREGORIAN_TO_LUNAR_IN_MODERN_NOTATION)
     {
-        int year = strtod(argv[1], NULL);
-        int month = strtod(argv[2], NULL);
-        int leap = strtod(argv[3], NULL);
-        int day = strtod(argv[4], NULL);
-        struct Lunar lunar = new_Lunar(year, month, leap, day, TZ);
-        if (!lunar.valid)
-        {
-            printf("Invalid Lunar date: %d-%d%s-%d\n", year, month, 
-                lunar.leap ? "i" : "", day);
-            return 1;
-        }
-        struct Gregorian date = convert_Lunar_to_Gregorian(lunar);
-        printf("%04d-%02d-%02d", date.year, date.month, date.day);
+        display_Lunar_in_modern_notation(argl);
+    }
+    else if (argl.task == LUNARH_LUNAR_TO_GREGORIAN)
+    {
+        display_Gregorian(argl);
+    }
+    else if (argl.task == LUNARH_LUNAR_CALENDAR_OF_A_YEAR)
+    {
+        display_Lunar_calendar_of_a_year(argl);
     }
     else
     {
-        printf("Invalid arguments\n");
+        printf("Unrecognizable task: %d\n", argl.task);
         return 1;
     }
 }
@@ -157,10 +130,78 @@ struct Gregorian convert_Lunar_to_Gregorian(struct Lunar lunar)
     return date;
 }
 
+void display_Lunar_in_Chinese(struct ArgumentList argl)
+{
+    struct Gregorian date = new_Gregorian(argl.year, argl.month, argl.day, 
+        argl.tz);
+    if (!date.valid)
+    {
+        printf("Invalid Gregorian date: %04d.%02d.%02d\n", argl.year, 
+            argl.month, argl.day);
+        exit(1);
+    }
+    struct Lunar lunar = convert_Gregorian_to_Lunar(date);
+    char out[256];
+    stringify_Lunar_date(lunar, out, 256);
+    printf("%s\n", out);
+}
+
+void display_Lunar_in_modern_notation(struct ArgumentList argl)
+{
+    struct Gregorian date = new_Gregorian(argl.year, argl.month, argl.day, 
+        argl.tz);
+    if (!date.valid)
+    {
+        printf("Invalid Gregorian date: %04d.%02d.%02d\n", argl.year, 
+            argl.month, argl.day);
+        exit(1);
+    }
+    struct Lunar lunar = convert_Gregorian_to_Lunar(date);
+    printf("%04d.%02d%s.%02d\n", lunar.year, lunar.month, 
+        lunar.leap ? "+" : "", lunar.day);
+}
+
+void display_Gregorian(struct ArgumentList argl)
+{
+    struct Lunar lunar = new_Lunar(argl.year, argl.month, argl.leap, argl.day,
+        argl.tz);
+    if (!lunar.valid)
+    {
+        printf("Invalid Lunar date: %04d.%02d%s.%02d\n", argl.year, 
+            argl.month, argl.leap ? "+" : "", argl.day);
+        exit(1);
+    }
+    struct Gregorian date = convert_Lunar_to_Gregorian(lunar);
+    printf("%04d.%02d.%02d\n", date.year, date.month, date.day);
+}
+
+void display_Lunar_calendar_of_a_year(struct ArgumentList argl)
+{
+    double tz = argl.tz;
+    int year = argl.year;
+    int last_jd = INT_MAX;
+    printf("Lunar\tGregorian\n");
+    for (int month = 1; month <= 12; month += 1)
+    {
+        struct Lunar lunar = new_Lunar(year, month, 0, 1, tz);
+        if (lunar.jd - last_jd > 30)
+        {
+            struct Lunar lunar = new_Lunar(year, month - 1, 1, 1, tz);
+            struct Gregorian date = convert_Lunar_to_Gregorian(lunar);
+            printf("%04d.%02d+.01\t%04d.%02d.%02d\n",
+                lunar.year, lunar.month, date.year, date.month, date.day);
+        }
+        struct Gregorian date = convert_Lunar_to_Gregorian(lunar);
+        printf("%04d.%02d.01\t%04d.%02d.%02d\n",
+            lunar.year, lunar.month, date.year, date.month, date.day);
+        last_jd = lunar.jd;
+    }
+}
+
 const char *get_branch_of_year(int year)
 {
     int branch = year % 12;
-    return BRANCHES[branch < 0 ? branch + 12 : branch];
+    return g_branches[branch < 0 ? branch + 12 : branch];
 }
 
 int get_kth_new_moon_jd(int k, double tz)
@@ -170,24 +211,25 @@ int get_kth_new_moon_jd(int k, double tz)
     double t3 = t2 * t;
     double jd1 = 2415020.75933 + 29.53058868 * k + 0.0001178 * t2 - 
         0.000000155 * t3 + 0.00033 * sin(
-            (166.56 + 132.87 * t - 0.009173 * t2) * DEGREES_TO_RADIANS
+            (166.56 + 132.87 * t - 0.009173 * t2) * g_degrees_to_radians
         );
     double m = 359.2242 + 29.10535608 * k - 0.0000333 * t2 - 0.00000347 * t3;
-    double mpr = 306.0253 + 385.81691806 * k + 0.0107306 * t2 + 0.00001236 * t3;
+    double mpr = 306.0253 + 385.81691806 * k + 0.0107306 * t2 + 
+        0.00001236 * t3;
     double f = 21.2964 + 390.67050646 * k - 0.0016528 * t2 - 0.00000239 * t3;
-    double c1 = (0.1734 - 0.000393 * t) * sin(m * DEGREES_TO_RADIANS) + 
-        0.0021 * sin(2 * m * DEGREES_TO_RADIANS) - 
-        0.4068 * sin(mpr * DEGREES_TO_RADIANS) + 
-        0.0161 * sin(2 * mpr * DEGREES_TO_RADIANS) -
-        0.0004 * sin(3 * mpr * DEGREES_TO_RADIANS) + 
-        0.0104 * sin(2 * f * DEGREES_TO_RADIANS) - 
-        0.0051 * sin((m + mpr) * DEGREES_TO_RADIANS) - 
-        0.0074 * sin((m - mpr) * DEGREES_TO_RADIANS) + 
-        0.0004 * sin((2 * f + m) * DEGREES_TO_RADIANS) - 
-        0.0004 * sin((2 * f - m) * DEGREES_TO_RADIANS) - 
-        0.0006 * sin((2 * f + mpr) * DEGREES_TO_RADIANS) + 
-        0.0010 * sin((2 * f - mpr) * DEGREES_TO_RADIANS) + 
-        0.0005 * sin((2 * mpr + m) * DEGREES_TO_RADIANS);
+    double c1 = (0.1734 - 0.000393 * t) * sin(m * g_degrees_to_radians) + 
+        0.0021 * sin(2 * m * g_degrees_to_radians) - 
+        0.4068 * sin(mpr * g_degrees_to_radians) + 
+        0.0161 * sin(2 * mpr * g_degrees_to_radians) -
+        0.0004 * sin(3 * mpr * g_degrees_to_radians) + 
+        0.0104 * sin(2 * f * g_degrees_to_radians) - 
+        0.0051 * sin((m + mpr) * g_degrees_to_radians) - 
+        0.0074 * sin((m - mpr) * g_degrees_to_radians) + 
+        0.0004 * sin((2 * f + m) * g_degrees_to_radians) - 
+        0.0004 * sin((2 * f - m) * g_degrees_to_radians) - 
+        0.0006 * sin((2 * f + mpr) * g_degrees_to_radians) + 
+        0.0010 * sin((2 * f - mpr) * g_degrees_to_radians) + 
+        0.0005 * sin((2 * mpr + m) * g_degrees_to_radians);
     double delta_t;
     if (t < -11)
     {
@@ -281,10 +323,8 @@ int get_num_days_of_Gregorian_month(int year, int month)
     case 9:
     case 11:
         return 30;
-
     case 2:
         return is_leap_Gregorian_year(year) ? 29 : 28;
-    
     default:
         return 31;
     }
@@ -293,22 +333,25 @@ int get_num_days_of_Gregorian_month(int year, int month)
 const char *get_stem_of_year(int value)
 {
     int stem = value % 10;
-    return STEMS[stem < 0 ? stem + 10 : stem];
+    return g_stems[stem < 0 ? stem + 10 : stem];
 }
 
 int get_sun_longitude(int jd, double tz)
 {
     double t = (jd - 2451545.5 - tz / 24.0) / 36525;
     double t2 = t * t;
-    double m = 357.52910 + 35999.05030 * t - 0.0001559 * t2 - 0.00000048 * t2 * t;
+    double m = 357.52910 + 35999.05030 * t - 0.0001559 * t2 - 
+        0.00000048 * t2 * t;
     double long0 = 280.46645 + 36000.76983 * t + 0.0003032 * t2;
     double dl = 
-        (1.914600 - 0.004817 * t - 0.000014 * t2) * sin(m * DEGREES_TO_RADIANS) + 
-        (0.019993 - 0.000101 * t) * sin(2 * m * DEGREES_TO_RADIANS) + 
-        0.000290 * sin(3 * m * DEGREES_TO_RADIANS);
-    double longitude = (long0 + dl) * DEGREES_TO_RADIANS;
-    longitude -= TAU * floor(longitude / TAU);
-    return (int)floor(longitude / PI * 6);
+        (1.914600 - 0.004817 * t - 0.000014 * t2) * sin(
+            m * g_degrees_to_radians
+        ) + 
+        (0.019993 - 0.000101 * t) * sin(2 * m * g_degrees_to_radians) + 
+        0.000290 * sin(3 * m * g_degrees_to_radians);
+    double longitude = (long0 + dl) * g_degrees_to_radians;
+    longitude -= g_tau * floor(longitude / g_tau);
+    return (int)floor(longitude / g_pi * 6);
 }
 
 int is_valid_Gregorian(int year, int month, int day)
@@ -330,9 +373,11 @@ struct Gregorian new_Gregorian(int year, int month, int day, double tz)
     int a = (14 - month) / 12;
     int y = year + 4800 - a;
     int m = month + 12 * a - 3;
-    int jd = day + (153 * m + 2) / 5 + 365 * y + y / 4 - y / 100 + y / 400 - 32045;
+    int jd = day + (153 * m + 2) / 5 + 365 * y + y / 4 - y / 100 + y / 400 
+        - 32045;
     struct Gregorian date = { 
-        .year = year, .month = month, .day = day, .jd = jd, .tz = tz, .valid = 1
+        .year = year, .month = month, .day = day, .jd = jd, .tz = tz, 
+        .valid = 1
     };
     return date;
 }
@@ -349,7 +394,8 @@ struct Gregorian new_Gregorian_2(int jd, double tz)
     int month = m + 3 - 12 * (m / 10);
     int year = b * 100 + d - 4800 + m / 10;
     struct Gregorian date = { 
-        .year = year, .month = month, .day = day, .jd = jd, .tz = tz, .valid = 1 
+        .year = year, .month = month, .day = day, .jd = jd, .tz = tz, 
+        .valid = 1 
     };
     return date;
 }
@@ -407,6 +453,75 @@ struct Lunar new_Lunar(int year, int month, int leap, int day, double tz)
     lunar.jd = jd;
     lunar.valid = 1;
     return lunar;
+}
+
+struct ArgumentList parse_argv(int argc, char **argv)
+{
+    struct ArgumentList argl = { 
+        .task = LUNARH_GREGORIAN_TO_LUNAR_IN_CHINESE, .tz = 7.0, .valid = 0,
+        .year = 0, .month = 0, .leap = 0, .day = 0
+    };
+    for (int i = 0; i < argc; i += 1)
+    {
+        if (strncmp("-y", argv[i], 3) == 0)
+        {
+            i += 1;
+            if (i == argc) { return argl; }
+            argl.year = strtol(argv[i], NULL, 10);
+        }
+        else if (strncmp("-m", argv[i], 3) == 0)
+        {
+            i += 1;
+            if (i == argc) { return argl; }
+            argl.month = strtol(argv[i], NULL, 10);
+        }
+        else if (strncmp("-d", argv[i], 3) == 0)
+        {
+            i += 1;
+            if (i == argc) { return argl; }
+            argl.day = strtol(argv[i], NULL, 10);
+        }
+        else if (strncmp("-tz", argv[i], 4) == 0)
+        {
+            i += 1;
+            if (i == argc) { return argl; }
+            argl.tz = strtod(argv[i], NULL);
+        }
+        else if (strncmp("-l", argv[i], 3) == 0)
+        {
+            argl.leap = 1;
+        }
+        else if (strncmp("-lm", argv[i], 4) == 0)
+        {
+            argl.task = LUNARH_GREGORIAN_TO_LUNAR_IN_MODERN_NOTATION;
+        }
+        else if (strncmp("-r", argv[i], 3) == 0)
+        {
+            argl.task = LUNARH_LUNAR_TO_GREGORIAN;
+        }
+        else if (strncmp("-ly", argv[i], 4) == 0)
+        {
+            argl.task = LUNARH_LUNAR_CALENDAR_OF_A_YEAR;
+        }
+    }
+    if (
+        argl.year == 0 && argl.month == 0 && argl.day == 0 && 
+        (
+            argl.task == LUNARH_GREGORIAN_TO_LUNAR_IN_CHINESE || 
+            argl.task == LUNARH_GREGORIAN_TO_LUNAR_IN_MODERN_NOTATION ||
+            argl.task == LUNARH_LUNAR_CALENDAR_OF_A_YEAR
+        )
+    )
+    {
+        time_t raw;
+        time(&raw);
+        struct tm* info = localtime(&raw);
+        argl.year = info->tm_year + 1900;
+        argl.month = info->tm_mon + 1;
+        argl.day = info->tm_mday;
+    }
+    argl.valid = 1;
+    return argl;
 }
 
 void stringify_Chinese_numeral(int value, char *out, int out_size)
